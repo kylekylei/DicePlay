2f7356d14db706c06481ed7d628ffbe4d0a743f8
Kyle <kylei.tw@gmail.ocm>
2021-09-02 09:03:09 +0800

    Initial Commit

---
+++ DicePlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DicePlay
+//
+//  Created by Kyle Lei on 2021/9/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
